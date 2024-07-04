4c996e779b725124064216f64baf8b0baef528eb
Kai Kuchenbecker <kuchenbecker.k@gmail.com>
2024-07-04 18:08:31 +0200

    Update enter-maintenance-mode.sh
    store scaled dynos into hidden file to use on leaving maintenance mode

--- enter-maintenance-mode.sh
+++ enter-maintenance-mode.sh
@@ -4,6 +4,17 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+function require() {
+  cmd=${1:?"Must pass program name"}
+  if ! command -v $cmd &> /dev/null; then
+      echo "$cmd could not be found, please install"
+      exit 1
+  fi
+}
+
+require heroku
+require jq
+
 USAGE='enter-maintenance-mode.sh [--debug] [-h|--help] [--add-dyno-type type ...] [--confirm boolean] [app_name ...]"
 
   Enter maintenance mode for provided apps. Will also scale all web dynos for those apps to zero.
@@ -22,6 +33,7 @@ USAGE='enter-maintenance-mode.sh [--debug] [-h|--help] [--add-dyno-type type ...
 APPS=()
 DYNO_TYPES=()
 CONFIRM=false
+ALL_DYNO_TYPES=false
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --debug) set -o xtrace; shift ;;
@@ -37,7 +49,7 @@ if [ ${#APPS[@]} -eq 0 ]; then
   APPS=${DEFAULT_APPS[@]}
 fi
 if [ ${#DYNO_TYPES[@]} -eq 0 ]; then
-  DYNO_TYPES=('web')
+  ALL_DYNO_TYPES=true
 fi
 
 if ! ${CONFIRM}; then
@@ -51,7 +63,16 @@ fi
 for app in "${APPS[@]}"; do
   echo "Putting ${app} in maintenance mode"
   heroku maintenance:on --app ${app}
-  for dyno_type in ${DYNO_TYPES[@]}; do
-    heroku ps:scale ${dyno_type}=0 --app ${app}
-  done
+  heroku ps --app ${app} --json | jq --raw-output 'map(.type)|group_by(.)[]|.[0] + "=" + (length|tostring)' > ".heroku_dyno_scales_before_for_${app}"
+  if ${ALL_DYNO_TYPES}; then
+    cat ".heroku_dyno_scales_before_for_${app}" \
+    | cut -d= -f1 \
+    | xargs -I{} heroku ps:scale {}=0 --app ${app}
+  else
+    for dyno_type in ${DYNO_TYPES[@]}; do
+      heroku ps:scale ${dyno_type}=0 --app ${app}
+    done
+    scaled_dynos=$(printf '%s\n' "${DYNO_TYPES[@]}" | grep -f - ".heroku_dyno_scales_before_for_${app}")
+    echo "${scaled_dynos}" > ".heroku_dyno_scales_before_for_${app}"
+  fi
 done
